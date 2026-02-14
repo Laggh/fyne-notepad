9c4ce65a46f3518344d279b7771963d11d01fd57
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2026-02-14 00:48:15 -0300

    Adicionado segurança para evitar que editor redimensione a janela

--- main.go
+++ main.go
@@ -15,8 +15,26 @@ func main() {
 
 	editorTheme := &EditorTheme{Theme: theme.DefaultTheme(), State: state}
 
-	w.SetContent(container.NewThemeOverride(state.Editor, editorTheme))
+	// container.New com sizeLimiterLayout "mente" o tamanho mínimo,
+	// impedindo a janela de explodir, mas entrega todo o espaço pro editor.
+	w.SetContent(container.New(&sizeLimiterLayout{},
+		container.NewThemeOverride(state.Editor, editorTheme)))
+
 	w.SetMainMenu(state.MenuBar)
 	w.Resize(fyne.NewSize(800, 600))
 	w.ShowAndRun()
+}
+
+// Layout simples para limitar o tamanho minimo reportado
+type sizeLimiterLayout struct{}
+
+func (l *sizeLimiterLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	for _, o := range objects {
+		o.Resize(size)
+		o.Move(fyne.NewPos(0, 0))
+	}
+}
+
+func (l *sizeLimiterLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	return fyne.NewSize(100, 100)
 }
